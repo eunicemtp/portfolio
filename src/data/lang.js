dc5a046c603e2369ca694e1dac11311191fb282a
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2025-07-19 07:49:20 +0200

    add lang switch + translate

--- src/data/lang.js
+++ src/data/lang.js
@@ -3,7 +3,7 @@ export const translations = {
     title: {
       home: "Accueil",
       about: "À propos",
-      work: "Travaux",
+      work: "Projet",
       contact: "Contact",
     },
     metaDescription: {
@@ -22,6 +22,47 @@ export const translations = {
           poster: "AFFICHES",
           hobby: "LOISIR",
         }
+      },
+      about:{
+        headline: "À propos de moi",
+        subheadlineBold: "Je m'appelle Eunice, designer et développeuse front-end ",
+        subheadline:"passionnée par l’accessibilité, la simplicité, et le pouvoir des idées bien pensées.",
+        content1:"J’ai grandi à Namur, une ville paisible qui a forgé ma personnalité curieuse et ancrée. Le français est ma langue maternelle, mais j’ai également étudié le néerlandais, et je parle anglais couramment grâce à des projets et expériences à l’international.",
+        content2:"Je suis diplômée en web design de la Haute École Albert Jacquard.J’aime concevoir des interfaces claires, inclusives et centrées sur l’humain. Je combine mes compétences en design et en code pour donner vie à des projets cohérents et accessibles.",
+        skills:"Compétences",
+        btn:"Télécharge mon CV",
+        searching: {
+          headline:"Ce que je recherche ?",
+          content1:"Je suis actuellement à la recherche de mon premier emploi en tant que développeur front-end. J'aime construire des interfaces propres, modernes et accessibles - de la structure HTML/CSS réfléchie aux micro-interactions fluides et aux mises en page réactives. J'aimerais rejoindre une équipe qui valorise la qualité du code, la collaboration et la précision visuelle.",
+          content2:"Ce que je recherche, c'est un espace où je puisse évoluer en tant que développeur front-end tout en mettant à profit mon œil de concepteur et ma sensibilité à l'interface utilisateur. Je veux contribuer à des projets importants, prendre le temps de bien faire les choses et explorer comment le code et la créativité peuvent travailler ensemble pour construire des expériences belles, efficaces et centrées sur l'humain.",
+        },
+        fun:{
+          headline:"Faits amusants ou non à mon sujet",
+          content1:"Je suis persuadée que la crème fraîche et les tomates ont été mariées à la naissance.Franchement, j’ai dû manger ce duo au moins 200 fois… et je ne suis clairement pas prête à m’arrêter.",
+          content2:"Je n’ai pas peur de mettre la main à la pâte — un jour, j’ai réparé une montre avec une pince à épiler et un optimisme aveugle.Elle donne encore l’heure… à peu près.",
+          content3:"Je suis convaincue que les meilleurs plans commencent par “On mange un truc ?” et se terminent par “Attends, il est déjà 2h du mat ?!”"
+        }
+        
+      },
+      work:{
+        headline:"Projet",
+        all:"Tous",
+        web:"site web",
+        poster:"affiche",
+        hobby:"loisir"
+
+      },
+      conatct:{
+        headline:"Contactez Moi",
+        firstname:"Prénom",
+        lastname:"Nom",
+        email:"E-mail",
+        object:"Objet",
+        message:"Message",
+        validation:"Merci ! Votre message a bien été envoyé.",
+        close:"Fermer",
+        btn:"Envoyer",
+
       }
     }
   },
@@ -48,7 +89,50 @@ export const translations = {
           poster: "POSTER DESIGN",
           hobby: "HOBBY",
         }
+      },
+      about:{
+        headline: "About me",
+        subheadlineBold: "I’m Eunice, a designer & front-end developer",
+        subheadline:"passionate about accessibility, simplicity, and the power of well-crafted ideas.",
+        content1:"I grew up in Namur, a quiet city that shaped my curious and grounded personality. French is my native language, but I also studied Dutch, and I speak English fluently thanks to international projects and experiences.",
+        content2:"I’m a web design graduate from Haute Ecole Albert Jacquard. I love creating clear, inclusive, human-centered interfaces. I combine my coding and design skills to bring cohesive and accessible projects to life.",
+        skills:"Skills",
+        btn:"Download my resume",
+        searching: {
+          headline:"What I’m looking for ?",
+          content1:"I’m currently looking for my first job as a front-end developer. I enjoy building clean, modern, and accessible interfaces — from thoughtful HTML/CSS structure to smooth micro-interactions and responsive layouts. I’d love to join a team that values quality code, collaboration, and visual precision.",
+          content2:"What I’m looking for is a space where I can grow as a front-end developer while leveraging my design eye and UI sensitivity. I want to contribute to projects that matter, take time to do things well, and explore how code and creativity can work together to build beautiful, efficient, and human-centered experiences.",
+        },
+        fun:{
+          headline:"Fun facts or not about me",
+          content1:"I believe crème fraîche and tomatoes are soulmates. I've probably eaten that combo 200 times and counting.",
+          content2:"I’m not afraid to get hands-on — I once fixed a watch with tweezers and blind optimism. It still tells time. Mostly.",
+          content3:"I believe the best plans start with “Let’s eat something” and end with “Wait, how is it 2AM?”"
+        }
+        
+      },
+      work:{
+        headline:"Work",
+        all:"all",
+        web:"website",
+        poster:"poster",
+        hobby:"hobby"
+
+      },
+      conatct:{
+        headline:"Contact Me",
+        firstname:"First Name",
+        lastname:"Last Name",
+        email:"E-mail",
+        object:"Object",
+        message:"Message",
+        validation:"Thank you! Your message has been sent.",
+        close:"Close",
+        btn:"Submit",
+
+
       }
+
     }
   },
 };
